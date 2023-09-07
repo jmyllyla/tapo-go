6585dafc413ea7e96c944640b109c274bbd7fac1
jmyllyla <jmyllyla@gmail.com>
2023-09-08 00:54:55 +0300

    Update cipher.go

--- cipher.go
+++ cipher.go
@@ -3,8 +3,6 @@ package tapo
 import (
 	"crypto/aes"
 	"crypto/cipher"
-
-	"github.com/mergermarket/go-pkcs7"
 )
 
 type Cipher struct {
@@ -12,11 +10,33 @@ type Cipher struct {
 	iv  []byte
 }
 
+func Pad(buf []byte, size int) ([]byte, error) {
+	bufLen := len(buf)
+	padLen := size - bufLen%size
+	padded := make([]byte, bufLen+padLen)
+	copy(padded, buf)
+	for i := 0; i < padLen; i++ {
+		padded[bufLen+i] = byte(padLen)
+	}
+	return padded, nil
+}
+
+func Unpad(padded []byte, size int) ([]byte, error) {
+	if len(padded)%size != 0 {
+		return nil, errors.New("pkcs7: Padded value wasn't in correct size.")
+	}
+
+	bufLen := len(padded) - int(padded[len(padded)-1])
+	buf := make([]byte, bufLen)
+	copy(buf, padded[:bufLen])
+	return buf, nil
+}
+
 func (c *Cipher) Encrypt(payload []byte) []byte {
 	block, _ := aes.NewCipher(c.key)
 	encrypter := cipher.NewCBCEncrypter(block, c.iv)
 
-	paddedPayload, _ := pkcs7.Pad(payload, aes.BlockSize)
+	paddedPayload, _ := Pad(payload, aes.BlockSize)
 	encryptedPayload := make([]byte, len(paddedPayload))
 	encrypter.CryptBlocks(encryptedPayload, paddedPayload)
 
@@ -30,7 +50,7 @@ func (c *Cipher) Decrypt(payload []byte) []byte {
 	decryptedPayload := make([]byte, len(payload))
 	encrypter.CryptBlocks(decryptedPayload, payload)
 
-	unpaddedPayload, _ := pkcs7.Unpad(decryptedPayload, aes.BlockSize)
+	unpaddedPayload, _ := Unpad(decryptedPayload, aes.BlockSize)
 
 	return unpaddedPayload
 }
